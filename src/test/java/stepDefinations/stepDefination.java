84c77eed21df243b6f9188ef5ada2c89ee8f3c3c
Mrinmoy Kumar Biswas <mrinmoy.blr@gmail.com>
2021-07-27 16:03:18 +0530

    New Commit 001

--- src/test/java/stepDefinations/stepDefination.java
+++ src/test/java/stepDefinations/stepDefination.java
@@ -2,6 +2,8 @@ package stepDefinations;
 
 import static io.restassured.RestAssured.given;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,46 +21,26 @@ import junit.framework.Assert;
 import static org.junit.Assert.*;
 import pojo.AddPlace;
 import pojo.Location;
+import resources.TestDataBuild;
+import resources.Utils;
 
-public class stepDefination {
+public class stepDefination extends Utils {
 
-	RequestSpecification req;
-	ResponseSpecification resSpec;
+	
 	RequestSpecification res;
+	ResponseSpecification resSpec;
 	Response response;
-	
-	
+	TestDataBuild data = new TestDataBuild();
 
 	@Given("Add Place Payload")
-	public void add_Place_Payload() {
-
-		AddPlace p = new AddPlace();
-		Location l = new Location();
-		p.setAccuracy(50);
-		p.setAddress("29, side layout, cohen 09");
-		p.setLanguage("French-IN");
-		p.setPhone_number("+91) 983 893 3937");
-		p.setWebsite("http://google.com");
-		p.setName("Frontline house");
-		List<String> myList = new ArrayList<String>();
-		myList.add("shoe park");
-		myList.add("shop");
-		p.setTypes(myList);
-		l.setLat(-38.383494);
-		l.setLng(33.427362);
-		p.setLocation(l);
-
-		req = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com").addQueryParam("Key", "qaclick123")
-				.setContentType(ContentType.JSON).build();
-
-		resSpec = new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
-
-		res = given().spec(req).body(p);
+	public void add_Place_Payload() throws IOException {		
+		res = given().spec(requestSpecification()).body(data.AddPlacePayload());
 
 	}
 
 	@When("User calls {string} with Post Http Request")
 	public void user_calls_with_Post_Http_Request(String string) {
+		resSpec = new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
 		response = res.when().post("/maps/api/place/add/json").then().spec(resSpec).extract().response();
 	}
 
@@ -66,16 +48,15 @@ public class stepDefination {
 	@Then("The API call got success with Status Code {int}")
 	public void the_API_call_got_success_with_Status_Code(Integer int1) {
 		Assert.assertEquals(response.getStatusCode(), 200);
-		assertEquals(response.getStatusCode(), 200);		
+		assertEquals(response.getStatusCode(), 200);
 	}
 
 	@Then("{string} in response body is {string}")
 	public void in_response_body_is(String KeyValue, String ExpectedValue) {
-		String resp=response.asString();
-		JsonPath js=new JsonPath(resp);		
+		String resp = response.asString();
+		JsonPath js = new JsonPath(resp);
 		System.out.println(js.getString(KeyValue).toString());
-		assertEquals(js.getString(KeyValue).toString(), ExpectedValue);		
+		assertEquals(js.getString(KeyValue).toString(), ExpectedValue);
 	}
-
 
 }
